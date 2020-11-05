b5334f2f7f4fb2b135ca72349d1c604ae3d21d94
Cameron <thejimmyblaze@gmail.com>
2020-11-05 21:00:33 +1300

    Merging ship changes.

---
+++ Moonshot-DeliveryInc/Assets/Scripts/Spaceship/Spaceship_Scroll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the scrolling speed of the Spaceship_Rig.
+/// Does NOT control the movement of the spaceship in relation to the camera.
+/// </summary>
+public class Spaceship_Scroll : MonoBehaviour
+{
+    [Range(-12f, 12f)]
+    public float Speed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Speed * Time.deltaTime;
+        transform.Translate(movement, 0, 0);
+    }
+}
---
+++ Moonshot-DeliveryInc/Assets/Scripts/Spaceship/Spaceship_Thruster.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the direction the thrusters are pointing in.
+/// </summary>
+public class Spaceship_Thruster : MonoBehaviour
+{
+    public float RotationModifier = -1f;
+    public float MinRotation = -90f;
+    public float MaxRotation = 90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateThruster();
+    }
+
+    // Rotates the object the script is attached to, the ammount of rotation is based on the speed of the ship.
+    // -10 degrees of rotation for each unit of speed.
+    // Min rotation -90. Max rotation 90.
+    // Rotation is multiplied by the RotationModifier.
+    private void RotateThruster()
+    {
+        float speed = GetShipSpeed();
+        float rotation = speed * RotationModifier;
+        rotation = Mathf.Clamp(rotation, MinRotation, MaxRotation);
+
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
+    }
+
+    // Gets the scroll speed of the Spaceship_Rig
+    private float GetShipSpeed()
+    {
+        Spaceship_Scroll scroll = transform.parent.parent.gameObject.GetComponent<Spaceship_Scroll>();
+        float speed = scroll.Speed;
+        return speed;
+    }
+}
