1a44d630168b399e2f4ca274f4c4515a9489b30f
Tim May <timmay1123@gmail.com>
2020-11-16 21:02:52 +1300

    despawn tiles

--- Moonshot-DeliveryInc/Assets/Scripts/TerrainController.cs
+++ Moonshot-DeliveryInc/Assets/Scripts/TerrainController.cs
@@ -7,28 +7,37 @@ public class TerrainController : MonoBehaviour
 
     public int levelLength = 10;
     private float spawnLocationX = 0.0f;
-    private float tileLength = 64 / 25;
-    //private float tileLength = 64f / 10.0f;
-    private int tilesOnScreen = 5;
+    private int tilesOnScreen = 6;
     public GameObject[] MoonTiles;
     private Transform spaceship;
     private int tilesSpawned = 0;
 
+    private List<GameObject> activeTiles;
+    private float baseTileSize;
+
     // Start is called before the first frame update
     private void Start()
     {
+        activeTiles = new List<GameObject>();
         spaceship = GameObject.Find("Spaceship_Rig").transform;
+        baseTileSize = MoonTiles[0].GetComponent<SpriteRenderer>().size.x; // Sort of need all the tiles to be the same size for this
 
+        //initial load
+        for (int i = 0; i < tilesOnScreen; i++)
+        {
+            SpawnTile();
+        }
 
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if (spaceship.position.x > spawnLocationX - tilesOnScreen * tileLength && tilesSpawned < levelLength) // 5 ~ tiles that fit on the screen
+        if (spaceship.position.x - 7 > (spawnLocationX - tilesOnScreen * baseTileSize) && tilesSpawned < levelLength) // 5 ~ tiles that fit on the screen
         {
             SpawnTile();
             tilesSpawned++;
+            DespawnTile();
         } 
     }
 
@@ -36,7 +45,14 @@ public class TerrainController : MonoBehaviour
     {
        
         var randomTileIndex = Random.Range(0, MoonTiles.Length);
-        Instantiate(MoonTiles[randomTileIndex], Vector3.right * spawnLocationX, Quaternion.identity, transform);
+        var tile = Instantiate(MoonTiles[randomTileIndex], Vector3.right * spawnLocationX, Quaternion.identity, transform);
         spawnLocationX += MoonTiles[randomTileIndex].GetComponent<SpriteRenderer>().size.x;
+        activeTiles.Add(tile);
+    }
+
+    private void DespawnTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
     }
 }
